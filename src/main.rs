e852245c11064c10a4583d6c10bcb0c03952d449
arijit1201 <arijit1201@gmail.com>
2022-03-13 14:17:10 +0530

    variables

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    let mut x: i32 = 1;
+    println!("the value of x is: {}", x);
+
+    x = 2;
+    println!("the value of x is: {}", x);
+
+    let y: bool = true;
+    println!("the value of y is {y}");
+
+    let y: bool = false;
+    println!("the value of y is {y}");
+
+    const CONSTANT_STRING: &str = "HELLO THERE";
+
+    println!("The value of the string constant is {CONSTANT_STRING}");
+
+
+    
 }
